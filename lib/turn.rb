6a1876170d28e742e72535f900328ada82a6fd15
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-20 10:13:49 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,40 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} ";
+end
+
+def input_to_index(user_input)
+  position = user_input.to_i
+  return position - 1
+end
+
+def valid_move?(board, index)
+  if (index.between?(0, 8))
+    if (board[index] == "" || board[index] == " ")
+      return true
+    end
+  else
+    return false
+  end
+
+end
+
+def move(board, index, mark = "X")
+  board[index] = mark
+end
+
+def turn(board)
+ puts "Please enter 1-9:"
+ user_input = gets
+ index = input_to_index(user_input)
+ if valid_move?(board, index)
+   move(board, index)
+   display_board(board)
+ else
+   until valid_move?(board, index)
+     puts "Please enter 1-9:"
+     user_input = gets
+     index = input_to_index(user_input)
+     valid_move?(board, index)
+   end
+ end
+end
